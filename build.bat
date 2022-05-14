27884a49820a2c4b7cefb9ef28f0bae96cb92595
Alexander Phillips <bigtear@vip.qq.com>
2022-05-14 10:08:23 +0800

    修改了打包脚本
    = = 无目的的

--- build.bat
+++ build.bat
@@ -1,8 +1,12 @@
 @echo off
 
 set DIR_PROJECT=%~dp0
+mkdir bin
 set PATH_RELEASE=%DIR_PROJECT%bin\CommandGo.exe
 
-go build -ldflags "-s -w" -o %PATH_RELEASE% %DIR_PROJECT%\cmd\main.go
-upx -9 %PATH_RELEASE%
+set GOOS=windows
+set GOARCH=386
+
+go.exe build -ldflags "-s -w" -o %PATH_RELEASE% %DIR_PROJECT%\cmd\main.go
+tools\upx.exe -q -9 %PATH_RELEASE%
 ls -lh %PATH_RELEASE%
